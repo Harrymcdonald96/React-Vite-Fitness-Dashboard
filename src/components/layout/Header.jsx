530896b219c11f3a3cc571d006e418e73618c82d
Harry Mcdonald <harryquirk96@icloud.com>
2024-08-14 15:41:27 +0100

    8

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,20 +4,21 @@ import '../../styles/Header.css';
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo-container">
-        <img src={`${import.meta.env.BASE_URL}src/assets/A-PEX.png`} alt="A-PEX Fitness Logo" />
-      </div>
-      <div className="vertical-divider"></div> 
-      <div className="welcome-message">
-        <span className="greeting">Good Morning!</span>
-        <span className="user-welcome">Welcome Back!</span>
-      </div>
-      <div className="user-info">
-        <img src={`${import.meta.env.BASE_URL}src/assets/AvatarPhoto.png`} alt="User Profile" className="profile-pic" />
-        <FiBell className="notification-icon" />
-        <FiSettings className="settings-icon" />
-      </div>
+    <div className="logo-container">
+    <img src="/assets/A-PEX.png" alt="A-PEX Fitness Logo" />
+</div>
+    <div className="vertical-divider"></div> 
+    <div className="welcome-message">
+      <span className="greeting">Good Morning!</span>
+      <span className="user-welcome">Welcome Back!</span>
     </div>
+    <div className="user-info">
+      <img src="/assets/AvatarPhoto.png" alt="User Profile" className="profile-pic" />
+      <FiBell className="notification-icon"/>
+      <FiSettings className="settings-icon"/>
+    </div>
+  </div>
+  
   );
 };
 
